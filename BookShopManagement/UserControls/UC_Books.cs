49a1466b5dbccbee438bcfb59e958b8658ad361a
Enes <enessolak99@gmail.com>
2020-05-26 01:08:42 +0300

    listings of books are implemented

--- BookShopManagement/UserControls/UC_Books.cs
+++ BookShopManagement/UserControls/UC_Books.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/**
+ * @author
+ * @date
+ * 
+ * @edited_by Enes Solak 26.05.2020
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -8,6 +15,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BookShopManagement.Forms;
+using System.Data.SqlClient;
+using BookShopManagement.Classes;
 
 namespace BookShopManagement.UserControls
 {
@@ -16,16 +25,105 @@ namespace BookShopManagement.UserControls
         public UC_Books()
         {
             InitializeComponent();
-            panel11.AutoScroll = true;
+            leftPanel.AutoScroll = true;
+
+            lblName.Text = "";
+            lblISBN.Text = "";
+            lblAuthor.Text = "";
+            lblPublisher.Text = "";
+            lblPage.Text = "";
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void UC_Books_Load(object sender, EventArgs e)
         {
+            SqlCommand command = new SqlCommand("SELECT * FROM TBLPRODUCT WHERE PTYPE=@P1", Connection.connect);
+            if (command.Connection.State != ConnectionState.Open)
+                command.Connection.Open();
+            command.Parameters.AddWithValue("@P1", "Book");
+            SqlDataReader reader = command.ExecuteReader();
+
+            int count = 0;
+            while (reader.Read())
+            {
+                Panel panel = new Panel
+                {
+                    BackColor = Color.FromArgb(39, 174, 96),
+                    Location = new Point(50 + count % 3 * 300, 50 + (int)(Math.Floor((decimal)count / 3) * 250)),
+                    Name = "product-" + reader["ID"].ToString(),
+                    Size = new Size(250, 200),
+                };
+
+                PictureBox pictureBox = new PictureBox
+                {
+                    Image = Utils.Base64StringToBitmap(reader["PICTURE"].ToString()),
+                    Location = new Point(155, 20),
+                    Name = "picture-" + reader["ID"].ToString(),
+                    Size = new Size(75, 100),
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    TabStop = false
+                };
+
+                Label name = new Label
+                {
+                    Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0))),
+                    ForeColor = Color.White,
+                    Location = new Point(20, 20),
+                    Name = "name-" + reader["ID"].ToString(),
+                    Size = new Size(145, 80),
+                    Text = reader["NAME"].ToString()
+                };
+
+                Label price = new Label
+                {
+                    AutoSize = true,
+                    Font = new Font("Century Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0))),
+                    ForeColor = Color.White,
+                    Location = new Point(20, 100),
+                    Name = "price-" + reader["ID"].ToString(),
+                    Size = new Size(145, 30),
+                    Text = reader["PRICE"].ToString() + " €"
+                };
+
+                Button button = new Button
+                {
+                    BackColor = Color.FromArgb(46, 204, 113),
+                    FlatStyle = FlatStyle.Flat,
+                    Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0))),
+                    ForeColor = Color.White,
+                    Location = new Point(20, 150),
+                    Name = "btnDetail-" + reader["ID"].ToString(),
+                    Size = new Size(210, 30),
+                    Text = "Show Details",
+                    UseVisualStyleBackColor = false,
+                    Cursor = Cursors.Hand
+                };
+                button.FlatAppearance.BorderSize = 0;
+                button.Click += OnPanelClick;
+
+                panel.Controls.Add(pictureBox);
+                panel.Controls.Add(price);
+                panel.Controls.Add(name);
+                panel.Controls.Add(button);
+
+                this.leftPanel.Controls.Add(panel);
+
+                count++;
+            }
+
+            reader.Close();
         }
 
-        private void panel2_Click(object sender, EventArgs e)
+        private void OnPanelClick (object sender, EventArgs e)
         {
-            lblISBN.Text = "Hello";
+            string[] name = ((Button)sender).Name.Split('-');
+
+            Book book = Book.CreateFromID(int.Parse(name[1]));
+
+            lblName.Text = book.Name;
+            lblISBN.Text = book.ISBN;
+            lblAuthor.Text = book.Author;
+            lblPublisher.Text = book.Publisher;
+            lblPage.Text = book.Page.ToString();
         }
     }
 }
