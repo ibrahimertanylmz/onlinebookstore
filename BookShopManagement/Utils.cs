c2701af5566de754a66f63c45e04cad5ec46c708
Enes <enessolak99@gmail.com>
2020-05-26 01:08:13 +0300

    add function for converting base64text to bitmap image

---
+++ BookShopManagement/Utils.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * @author Enes Solak 
+ * @date 25.05.2020
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopManagement
+{
+    class Utils
+    {
+        public static Bitmap Base64StringToBitmap(string base64String)
+        {
+            Bitmap bmpReturn = null;
+            //Convert Base64 string to byte[]
+            byte[] byteBuffer = Convert.FromBase64String(base64String);
+            MemoryStream memoryStream = new MemoryStream(byteBuffer);
+            memoryStream.Position = 0;
+
+            bmpReturn = (Bitmap)Bitmap.FromStream(memoryStream);
+
+            memoryStream.Close();
+            memoryStream = null;
+            byteBuffer = null;
+
+            return bmpReturn;
+        }
+    }
+}
