22f9f2cb5446eccf96053fd4b556a7e19bf0b569
152120171050 <152120171050@ogrenci.ogu.edu.tr>
2020-05-25 20:32:02 +0300

    createfromdatabase

--- BookShopManagement/Classes/Book.cs
+++ BookShopManagement/Classes/Book.cs
@@ -5,6 +5,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,12 +20,45 @@ namespace BookShopManagement.Classes
         public string Publisher { get; protected set; }
         public int Page { get; protected set; }
 
-        public Book(string ISBN, string Author, string Publisher, int Page)
+        public Book(int Id, string Name,  double Price, int Quantity, string Picture, string ISBN, string Author, string Publisher, int Page)
         {
             this.ISBN = ISBN;
             this.Author = Author;
             this.Publisher = Publisher;
             this.Page = Page;
+            this.Name = Name;
+            this.Id = Id;
+            this.Price = Price;
+            this.Quantity = Quantity;
+            this.Picture = Picture;
+        }
+
+        public static Book CreateFromID(int ID)
+        {
+            SqlCommand command = new SqlCommand("select * FROM TBLPRODUCT WHERE ID=@P1", Connection.connect);
+            if (command.Connection.State != ConnectionState.Open)
+            {
+                command.Connection.Open();
+            }
+            command.Parameters.AddWithValue("@P1", ID);
+
+            SqlDataReader dr = command.ExecuteReader();
+            if (dr.Read())
+            {
+                return new Book(
+                    Int32.Parse(dr[0].ToString()),
+                    dr[2].ToString(),
+                    double.Parse(dr[3].ToString()),
+                    Int32.Parse(dr[4].ToString()),
+                    dr[5].ToString(),
+                    dr[6].ToString(),
+                    dr[7].ToString(),
+                    dr[8].ToString(),
+                    Int32.Parse(dr[9].ToString())
+                    );
+            }
+            dr.Close();
+            throw new Exception();
         }
     }
 }
--- BookShopManagement/Classes/Product.cs
+++ BookShopManagement/Classes/Product.cs
@@ -14,14 +14,14 @@ namespace BookShopManagement.Classes
     public abstract class Product
     {
         public string Name { get; protected set; }
-        public string Id { get; protected set; }
+        public int Id { get; protected set; }
         public double Price { get; protected set; }
         public int Quantity { get; protected set; }
         public string Picture { get; protected set; }
 
         protected Product() { }
 
-        protected Product(string Name, string Id, double Price, int Quantity, string Picture)
+        protected Product(string Name, int Id, double Price, int Quantity, string Picture)
         {
             this.Name = Name;
             this.Id = Id;
