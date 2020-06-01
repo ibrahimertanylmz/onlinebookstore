7adac6bc32c201fda588207431eeebd144770594
Ahmad Zaidan <152120171076@ogrenci.ogu.edu.tr>
2020-06-01 16:30:01 +0300

    Comments for Classes

--- BookShopManagement/Connection.cs
+++ BookShopManagement/Connection.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/**
+ * @author Onur Guzel
+ * @date 25.05.2020
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +11,9 @@ using System.Data.SqlClient;
 
 namespace BookShopManagement
 {
+    /**
+    * \brief A class that initiates the connection with the database
+    */
     public class Connection
     {
         public static SqlConnection connect = new SqlConnection("workstation id=bookstore.mssql.somee.com;packet size=4096;user id=trname_SQLLogin_1;pwd=992qrgsk3z;data source=bookstore.mssql.somee.com;persist security info=False;initial catalog=bookstore");
